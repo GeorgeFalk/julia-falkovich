122884e237cddbeea34b9fa7a504f2cc0aea2b2b
unknown <gfalk85@gmail.com>
2019-10-10 13:20:59 +0300

    css i

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -37,7 +37,7 @@ export default class extends Component {
           <div className="row mx-5">
             <div className="col text-center my-0">
               <p
-                className="logo-font mb-0 text-center mx-auto"
+                className="logo-font mb-0 text-center mx-auto logo-res"
                 style={{ "font-size": "3rem" }}
               >
                 Julia Falkovich
