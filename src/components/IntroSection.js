5ab7846106162d581c2e0e90fa165230cc8f7505
unknown <gfalk85@gmail.com>
2019-10-10 13:10:03 +0300

    css i

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -37,7 +37,7 @@ export default class IntroSection extends Component {
         <section id="intro-section">
           <div className="container-fluid">
             <div className="row align-items-center ">
-              <div className="col-lg-5 text-center intro-info d-flex align-items-center justify-content-center">
+              <div className="col-lg-5 text-center intro-info d-flex justify-content-center">
                 <div ref="box1">
                   <Switch>
                     <Route exact path="/" component={Intro} />
